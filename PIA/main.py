f6f54065eaaa0183d7c20889ca5d38261254a9de
OrbitalCardinal <raulcepedac1@gmail.com>
2022-05-26 16:28:40 -0500

    Modificación 2do método alfred ivan

--- PIA/main.py
+++ PIA/main.py
@@ -1,17 +1,14 @@
 from segmentation import main as blasco
 from PIAVC import main as lopezgarcia
-from mainf import main as pydipati
 print('1. Blasco 2007 . Detección de defectos en las cascaras de frutas con segmentación. (Segmentación basada en color)')
 print('2. Lopez Garcia 2010. Detección de defectos en la superficie de las frutas con análisis de textura (Segmentación basado en textura).')
 print('3. Pydipati 2006. Demo de detección de daño por enfermedad en los citricos con caracteristicas de color y texuta (Segmentación basado en color y textura).')
 option = int(input('Ingrese la opción del método a aplicar: '))
 if option == 1:
-    path = input('Ingrese la ruta de la imagen: ')
-    blasco(path)
+    blasco('orange_blasco.png')
 elif option == 2:
-    path = input('Ingrese la ruta de la imagen: ')
-    lopezgarcia(path)
+    lopezgarcia('orange_blasco.png')
 elif option == 3:
-    pydipati()
+    from mainf import main as pydipati
 else:
     print('Opción invalida')
--- PIA/mainf.py
+++ PIA/mainf.py
@@ -1,30 +1,22 @@
-def main():
-    #!/usr/bin/env python
-    """
-    Main execution file.
-    """
+#!/usr/bin/env python
+"""
+Main execution file.
+"""
 
-    import first_task
-    import second_task
-    import final_challenge
+import second_task
 
-    __author__ = "Marco Rossini"
-    __copyright__ = "Copyright 2020, Marco Rossini"
-    __date__ = "2020/05"
-    __license__ = "MIT"
-    __version__ = "1.0"
 
-    # ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
 
-    # Run first task
-    print("----- Running first task -----")
-    first_task.run()
+# Run first task
+#print("----- Running first task -----")
+#first_task.run()
 
-    # Run second task
-    print("\n----- Running second task -----")
-    second_task.run_clustering()
-    second_task.run_samples()
+# Run second task
+print("\n----- Ejecutando metodo -----")
+#second_task.run_clustering()
+second_task.run_samples()
 
-    # Run final challenge
-    print("\n----- Running final challenge -----")
-    final_challenge.run()
+# Run final challenge
+#print("\n----- Running final challenge -----")
+#final_challenge.run()
